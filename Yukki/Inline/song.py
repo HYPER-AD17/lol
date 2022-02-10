778232085ddf7e3be4c402a12b50106ba23fe474
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-02-11 04:25:06 +0530

    Update song.py

--- Yukki/Inline/song.py
+++ Yukki/Inline/song.py
@@ -6,21 +6,21 @@ def song_markup(videoid, duration, user_id, query, query_type):
     buttons = [
         [
             InlineKeyboardButton(
-                text="❮",
+                text="❮𝗣𝗥𝗘𝗩",
                 callback_data=f"song_right B|{query_type}|{query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="Download",
+                text="𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗",
                 callback_data=f"qwertyuiopasdfghjkl {videoid}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="❯",
+                text="𝗡𝗘𝗫𝗧❯",
                 callback_data=f"song_right F|{query_type}|{query}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Close Search",
+                text="🗑 𝗖𝗟𝗢𝗦𝗘𝄡",
                 callback_data=f"forceclose {query}|{user_id}",
             )
         ],
@@ -32,17 +32,17 @@ def song_download_markup(videoid, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⬇️ Get Audio",
+                text="⬇️ 𝗔𝗨𝗗𝗜𝗢!!",
                 callback_data=f"gets audio|{videoid}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⬇️ Get Video",
+                text="⬇️ 𝗩𝗜𝗗𝗘𝗢!!",
                 callback_data=f"gets video|{videoid}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Close Menu",
+                text="🗑 𝗖𝗟𝗢𝗦𝗘",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
