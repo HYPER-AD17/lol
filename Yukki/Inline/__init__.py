705d51e495ef81e0431ae60e46336b00dbf329bf
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-03-11 00:28:27 +0530

    Update __init__.py

--- Yukki/Inline/__init__.py
+++ Yukki/Inline/__init__.py
@@ -1,7 +1,7 @@
 from .others import download_markup, others_markup
 from .play import (audio_markup, audio_markup2, audio_markup202, audio_markup201, audio_timer_markup_start,
                    primary_markup, search_markup, search_markup2,
-                   secondary_markup, timer_markup, url_markup, url_markup2)
+                   secondary_markup, timer_markup, url_markup)
 from .playlist import (add_genre_markup, check_genre_markup, check_markup,
                        delete_playlist_markuup, fetch_playlist,
                        paste_queue_markup, play_genre_playlist,
@@ -10,3 +10,5 @@ from .song import song_download_markup, song_markup
 from .start import (custommarkup, dashmarkup, private_panel, setting_markup,
                     setting_markup2, start_pannel, usermarkup, volmarkup)
 from .stats import stats1, stats2, stats3, stats4, stats5, stats6, stats7
+
+# Dᴇᴠᴇʟᴏᴘᴇʀ💫   url_markup2
