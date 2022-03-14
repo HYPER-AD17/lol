9c6b0178e19e9da7cef859ce99b25cbdca758a14
™NixonHype® <92142624+HYPER-AD17@users.noreply.github.com>
2022-03-14 22:34:09 +0530

    Update song.py

--- Yukki/Inline/song.py
+++ Yukki/Inline/song.py
@@ -6,27 +6,25 @@ def song_markup(videoid, duration, user_id, query, query_type):
     buttons = [
         [
             InlineKeyboardButton(
-                text="𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗",
-                callback_data=f"qwertyuiopasdfghjkl {videoid}|{user_id}",),
-        ],
-        [
-            InlineKeyboardButton(
-                text="ᴘʀᴇᴠ",
+                text="««",
                 callback_data=f"song_right B|{query_type}|{query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="ɴᴇxᴛ",
+                text="ᴅᴏᴡɴʟᴏᴀᴅ",
+                callback_data=f"qwertyuiopasdfghjkl {videoid}|{user_id}",),
+            InlineKeyboardButton(
+                text="»»",
                 callback_data=f"song_right F|{query_type}|{query}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🗑𝗖𝗟𝗢𝗦𝗘𝄢",
-                callback_data=f"forceclose {query}|{user_id}",
-            ),
-            InlineKeyboardButton(
                 text="Dᴇᴠᴇʟᴏᴘᴇʀ💫",
                 url="https://t.me/SILENT_DEVS",
+            ),
+            InlineKeyboardButton(
+                text="🗑ᴄʟᴏsᴇ",
+                callback_data=f"forceclose {query}|{user_id}",
             ),
         ],
     ]
@@ -37,22 +35,22 @@ def song_download_markup(videoid, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⬇️ 𝗔𝗨𝗗𝗜𝗢!!",
+                text="🎵ᴀᴜᴅɪᴏ!!",
                 callback_data=f"gets audio|{videoid}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⬇️ 𝗩𝗜𝗗𝗘𝗢!!",
+                text="🎬ᴠɪᴅᴇᴏ!!",
                 callback_data=f"gets video|{videoid}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🗑 𝗖𝗟𝗢𝗦𝗘",
-                callback_data=f"forceclose {videoid}|{user_id}",
-            ),
-            InlineKeyboardButton(
                 text="Dᴇᴠᴇʟᴏᴘᴇʀ💫",
                 url="https://t.me/SILENT_DEVS",
+            ),
+            InlineKeyboardButton(
+                text="🗑ᴄʟᴏsᴇ",
+                callback_data=f"forceclose {query}|{user_id}",
             ),
         ],
     ]
